170d25cdced3631177223979ae6560ee7b03d17a
Sabeer Sulaiman <sabeersulaimanpv@gmail.com>
2019-07-18 10:41:31 +0530

    Spring Initializer code

---
+++ src/main/java/com/omnicuris/ecommercebasic/ECommerceBasicApplication.java
@@ -0,0 +1,13 @@
+package com.omnicuris.ecommercebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBasicApplication.class, args);
+	}
+
+}
